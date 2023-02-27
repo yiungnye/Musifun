e0f5edf9d5c56636748d08adf3e6b17d042064a4
Havish Soowamber <soowamhc@gmail.com>
2023-02-27 15:54:38 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'instruments/index'
-  get 'instruments/show'
-  get 'instruments/new'
-  get 'instruments/create'
   devise_for :users
   root to: "instruments#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :instruments
 end
