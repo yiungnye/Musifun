13e4430fa39508c40de729d48855a7515ab730c2
Havish Soowamber <soowamhc@gmail.com>
2023-02-28 15:51:42 +0400

    Added model & controller for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params_review)
+    @instrument = Instrument.find(params[:id])
+    @review.booking.instrument = @instrument
+    @review.booking.user = current_user
+
+    if @review.save
+      redirect_to instrument_path(@instrument)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:rating, :comment)
+  end
+end
