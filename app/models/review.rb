9c6b5bfffda6d1e7950b42d5539da45218d898a2
Havish Soowamber <soowamhc@gmail.com>
2023-03-01 15:36:34 +0400

    Fixed model Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  belongs_to :instrument, through: :booking
-  belongs_to :user, through: :booking
+  # belongs_to :instrument, through: :booking
+  # belongs_to :user, through: :booking
 
   validates :rating, presence: true
 end
