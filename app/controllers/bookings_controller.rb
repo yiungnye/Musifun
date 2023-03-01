f52f39195e1b21094953ff38a6012e0bc254b9e1
ezekielramtano <codingwithezekiel@gmail.com>
2023-03-01 13:34:26 +0400

    done with created and new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,18 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @user = current_user
+    @booking = Booking.new(params_booking)
+    @booking.user = @user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
