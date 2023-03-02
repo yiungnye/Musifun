b2363ede3b493cfd781e4963c7f9ffb319f3fd6b
Havish Soowamber <soowamhc@gmail.com>
2023-03-02 11:11:06 +0400

    fixed routes again

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: "instruments#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :instruments
-
-  resources :bookings, only: %i[index create show] do
-    resources :reviews, only: %i[create]
+  resources :instruments do
+    resources :bookings, only: %i[index create show]
   end
 end
